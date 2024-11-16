8dc129afc0dce6d05c52506a97b5279eb680b22e
azikenp <azikenp26@gmail.com>
2024-11-16 22:38:53 +0100

    done with cryptocurrencies component

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useGetCryptoQuery } from "../services/cryptoApi";
+import { Card, Col, Input, Row } from "antd";
+import { Link } from "react-router-dom";
+import millify from "millify";
 
-const Cryptocurrencies = () => {
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const [searchTerm, setSearchTerm] = useState("");
+  const { data: cryptoList, isFetching } = useGetCryptoQuery(count);
+  const [cryptos, setCryptos] = useState([]);
+
+  useEffect(() => {
+    const filterData = cryptoList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setCryptos(filterData);
+  }, [cryptoList, searchTerm]);
+
+  if (isFetching) return "Loading...";
+
   return (
-    <div>Cryptocurrencies</div>
-  )
-}
+    <>
+      <div className="search-crypto">
+        {!simplified && (
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        )}
+      </div>
 
-export default Cryptocurrencies
+      <Row gutter={[32, 32]} className="crypto-card-component">
+        {cryptos?.map((currency) => (
+          <Col
+            key={currency.uuid}
+            xs={24}
+            sm={12}
+            lg={6}
+            className="crypto-card"
+          >
+            <Link to={`/crypto/${currency.uuid}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default Cryptocurrencies;
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,7 +8,7 @@ import { Cryptocurrencies, News } from "../components";
 const { Title } = Typography;
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptoQuery();
+  const { data, isFetching } = useGetCryptoQuery(10);
   console.log(data);
 
   const globalStats = data?.data?.stats;
