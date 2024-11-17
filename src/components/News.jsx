5349986eb722b14f434df5fc20ada776b866af3b
azikenp <azikenp26@gmail.com>
2024-11-17 20:48:55 +0100

    news page done

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,21 +1,77 @@
-import { Typography } from "antd";
+import { Avatar, Card, Col, Row, Select, Typography } from "antd";
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+import moment from "moment";
+import { useState } from "react";
 
 const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState("coindesk");
   const { Text, Title } = Typography;
-  const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({
-    newsCategory: "cryptodaily",
-    // count: simplified ? 10 : 100,
-  });
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory });
+
+  const demoImage =
+    "https://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg";
 
   console.log(cryptoNews);
 
-  if (isFetching) return "Loading...";
+  if (!cryptoNews) return "Loading...";
+
+  const displayedNews = simplified
+    ? cryptoNews.data.slice(0, 6)
+    : cryptoNews.data.slice(0, 20);
 
   return (
-    <>
-      <div></div>
-    </>
+    <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a news platform"
+            optionFilterProp="children"
+            onChange={(value) => console.log(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().index(input.toLowerCase()) >= 0
+            }
+          >
+            hi
+          </Select>
+        </Col>
+      )}
+
+      {displayedNews.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.title}
+                </Title>
+                <img
+                  src={news.thumbnail ? news.thumbnail : demoImage}
+                  alt="news image"
+                  className="img"
+                />
+              </div>
+              <p>
+                {news.description > 100
+                  ? `${news.description.substring(0, 100)} ... `
+                  : news.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar
+                    src={news.thumbnail ? news.thumbnail : demoImage}
+                    alt="news"
+                  />
+                  <Text className="provider-name">Mohamed Jabar</Text>
+                </div>
+                <Text>{moment(news.createdAt).startOf("ss").fromNow()}</Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   );
 };
 
