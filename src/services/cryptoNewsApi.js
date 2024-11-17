ee00c5dd790464c2c0afdb9b64171edfbc7ed979
azikenp <azikenp26@gmail.com>
2024-11-17 08:12:44 +0100

    addiitons

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,1 +1,26 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeader = {
+  "x-rapidapi-key": "8d5d2a2cd3msha12dddadeb22130p1b8b49jsnc231130cff9f",
+  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+  "X-BingApis-SDK": "true",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com/";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeader });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
