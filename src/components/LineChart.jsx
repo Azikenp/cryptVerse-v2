0da76b102055a1620961af3c8b4701553184d11f
azikenp <azikenp26@gmail.com>
2024-11-18 22:05:44 +0100

    chart almost done

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -9,12 +9,39 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
 
   const coinPrice = [];
   const coinTimestamp = [];
+
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    coinPrice.push(coinHistory.data.history[1].price);
+    coinPrice.push(coinHistory.data.history[i].price);
     coinTimestamp.push(
-      new Date(coinHistory.data.history[1].timestamp).toLocaleDateString()
+      new Date(coinHistory.data.history[i].timestamp).toLocaleDateString()
     );
   }
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: "Price In USD",
+        data: coinPrice,
+        fill: false,
+        backgroundColor: "#0071bd",
+        borderColor: "#0071bd",
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
   return (
     <>
       <Row className="chart-header">
